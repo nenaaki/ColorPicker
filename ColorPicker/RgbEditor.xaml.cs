1035e9c75aab9b985009f0b0581bb56e1a014a3e
nenaaki <nenaaki@gmail.com>
2019-06-12 12:59:37 +0900

    色入力部作成

---
+++ ColorPicker/RgbEditor.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace ColorPicker
+{
+    /// <summary>
+    /// RgbEditor.xaml の相互作用ロジック
+    /// </summary>
+    public partial class RgbEditor : UserControl
+    {
+        public byte Red
+        {
+            get { return (byte)GetValue(RedProperty); }
+            set { SetValue(RedProperty, value); }
+        }
+        public static readonly DependencyProperty RedProperty
+            = DependencyProperty.Register(nameof(Red), typeof(byte), typeof(RgbEditor), new PropertyMetadata((byte)0,
+            (d, e) => ((RgbEditor)d).SyncColor(false)));
+
+        public byte Blue
+        {
+            get { return (byte)GetValue(BlueProperty); }
+            set { SetValue(BlueProperty, value); }
+        }
+        public static readonly DependencyProperty BlueProperty
+            = DependencyProperty.Register(nameof(Blue), typeof(byte), typeof(RgbEditor), new PropertyMetadata((byte)0,
+            (d, e) => ((RgbEditor)d).SyncColor(false)));
+
+        public byte Green
+        {
+            get { return (byte)GetValue(GreenProperty); }
+            set { SetValue(GreenProperty, value); }
+        }
+        public static readonly DependencyProperty GreenProperty
+            = DependencyProperty.Register(nameof(Green), typeof(byte), typeof(RgbEditor), new PropertyMetadata((byte)0,
+            (d, e) => ((RgbEditor)d).SyncColor(false)));
+
+        public Color CurrentColor
+        {
+            get { return (Color)GetValue(CurrentColorProperty); }
+            set { SetValue(CurrentColorProperty, value); }
+        }
+        public static readonly DependencyProperty CurrentColorProperty
+            = DependencyProperty.Register(nameof(CurrentColor), typeof(Color), typeof(RgbEditor), new PropertyMetadata(Colors.Black,
+            (d, e) => ((RgbEditor)d).SyncColor(true)));
+
+        public RgbEditor()
+        {
+            InitializeComponent();
+        }
+
+        private bool colorUpdating;
+
+        private void SyncColor(bool currentChanged)
+        {
+            if (colorUpdating)
+                return;
+
+            try
+            {
+                colorUpdating = true;
+                if (currentChanged)
+                {
+                    var current = CurrentColor;
+                    Red = current.R;
+                    Green = current.G;
+                    Blue = current.B;
+                }
+                else
+                {
+                    CurrentColor = Color.FromRgb(Red, Green, Blue);
+                }
+            }
+            finally
+            {
+                colorUpdating = false;
+            }
+        }
+    }
+}
--- Sample/MainWindow.xaml.cs
+++ Sample/MainWindow.xaml.cs
@@ -3,6 +3,8 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Shapes;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace Sample
 {
@@ -19,8 +21,32 @@ namespace Sample
         }
     }
 
-    public class MainWindowViewModel
+    public class MainWindowViewModel : INotifyPropertyChanged
     {
         public Color[] BaseColors => new Color[] { Colors.White, Colors.Black, Colors.Red, Colors.Blue, Colors.Green, Colors.LightGreen, Colors.Pink, Colors.SkyBlue };
+
+        private Color _baseColor;
+        public Color BaseColor
+        {
+            get => _baseColor;
+            set => UpdateProperty(ref _baseColor, value);
+        }
+
+        private Color _currentColor;
+        public Color CurrentColor
+        {
+            get => _currentColor;
+            set => UpdateProperty(ref _currentColor, value);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void UpdateProperty<T>(ref T field, T value, [CallerMemberName]string propertyName = null, string[] dependedProperties = null)
+        {
+            field = value;
+            OnPropertyChanged(propertyName);
+        }
+
+        public void OnPropertyChanged([CallerMemberName]string propertyName = null) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 }
