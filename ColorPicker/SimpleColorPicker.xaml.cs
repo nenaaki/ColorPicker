8e3b5372cdf7b0a57fb0fdf92aff7ce056b035c7
nenaaki <nenaaki@gmail.com>
2019-06-16 12:27:42 +0900

    シンプルなカラーピッカーを追加

---
+++ ColorPicker/SimpleColorPicker.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace ColorPicker
+{
+    /// <summary>
+    /// SimpleColorPicker.xaml の相互作用ロジック
+    /// </summary>
+    public partial class SimpleColorPicker : ListBox
+    {
+        public SimpleColorPicker()
+        {
+            InitializeComponent();
+        }
+    }
+}
