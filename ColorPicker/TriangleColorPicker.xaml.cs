4180f89c4429c84e4abea757c0fc598ee69df385
nenaaki <nenaaki@gmail.com>
2019-07-01 16:44:57 +0900

    三角形ピッカーの調整

--- ColorPicker/TriangleColorPicker.xaml.cs
+++ ColorPicker/TriangleColorPicker.xaml.cs
@@ -13,16 +13,6 @@ namespace ColorPicker
     public partial class TriangleColorPicker : HsvColorPickerBase
     {
         private readonly SolidColorBrush _brush = new SolidColorBrush();
-        private readonly LinearGradientBrush _brushBaseColor = new LinearGradientBrush()
-        {
-            StartPoint = new Point(0, 1),
-            EndPoint = new Point(0.4, 0.2),
-            GradientStops = new GradientStopCollection(new[]
-            {
-                new GradientStop(Colors.Black, 0),
-                new GradientStop(Colors.Transparent, 1.0), // MEMO : BaseColorを入れる
-            })
-        };
 
         private Updater _colorUpdater = new Updater();
 
@@ -30,8 +20,6 @@ namespace ColorPicker
         {
             InitializeComponent();
             _image.Source = MakeSvRect();
-            Pointer.Fill = _brush;
-            _baseColor.Fill = _brushBaseColor;
 
             SyncColor(null);
             MouseDown += TriangleColorPicker_MouseDown;
@@ -60,8 +48,7 @@ namespace ColorPicker
 
         private void UpdateCurrentColorFromCanvas(Point position)
         {
-            System.Diagnostics.Debug.WriteLine("P " + position.ToString());
-            CurrentColor = ToColor(100, 100, position, BaseColor);
+            CurrentColor = ToColor(_canvas.Width, _canvas.Height, position, BaseColor);
         }
 
         private (double s, double v) ToSV(double width, double height, Point location)
@@ -69,7 +56,7 @@ namespace ColorPicker
             var x = location.X / width;
             var y = location.Y / height;
 
-            var v = Math.Max(0, Math.Min(1, (1 - y) + x / 2));
+            var v = Math.Max(0, Math.Min(1, 1 - y + x / 2));
             var s = Math.Max(0, Math.Min(1, y + x / 2));
             return (s, v);
         }
@@ -81,43 +68,52 @@ namespace ColorPicker
             return new HsvColor(HsvColor.FromColor(BaseColor).H, s, v).ToRgb();
         }
 
+        /// <summary>
+        /// 色から三角形ピッカー上の座標を計算します。
+        /// </summary>
         private Point ToLocation(double width, double height, Color color)
         {
             var hsvColor = HsvColor.FromColor(color);
             var s = hsvColor.S;
             var v = hsvColor.V;
+            if (s == 0 && v == 0)
+                s = 1.0;
 
-            double x = s * width * v;
-            double y = (1.0 - Math.Max(0.0, Math.Min(1.0, v * (1.0 - s)))) * height - x / 2;
-
-            System.Diagnostics.Debug.WriteLine(new Point(x, y).ToString());
-            return new Point(x, y);
+            return new Point(
+                width * Math.Max(0, Math.Min(1, s - 1 + v)),
+                height * Math.Max(0, Math.Min(1, (s + 1 - v) / 2)));
         }
 
+        /// <summary>
+        /// SV色空間の矩形を作ります。
+        /// </summary>
         private BitmapSource MakeSvRect()
         {
-            const int height = 128;
-            const int width = 128;
+            const int BMP_WIDTH = 128;
+            const int BMP_HEIGHT = 128;
 
-            var wb = new WriteableBitmap(width, height, 96, 96, PixelFormats.Bgra32, null);
+            var wb = new WriteableBitmap(BMP_WIDTH, BMP_HEIGHT, 96, 96, PixelFormats.Bgra32, null);
             int stride = wb.BackBufferStride;
-            byte[] pixels = new byte[height * stride];
+            byte[] pixels = new byte[BMP_HEIGHT * stride];
 
-            for (int y = 0; y < height; y++)
+            for (int y = 0; y < BMP_HEIGHT; y++)
             {
-                for (int x = 0; x < width; x++)
+                for (int x = 0; x < BMP_WIDTH; x++)
                 {
                     var p = y * stride + x * 4;
-                    var (s, v) = ToSV(width, height, new Point(x, y));
+                    var (s, v) = ToSV(BMP_WIDTH, BMP_HEIGHT, new Point(x, y));
 
                     pixels[p + 2] = pixels[p + 1] = pixels[p] = (byte)Math.Min(255, Math.Max(0, v * 255));
                     pixels[p + 3] = (byte)Math.Min(255, Math.Max(0, (1.0 - s) * 255));
                 }
             }
-            wb.WritePixels(new Int32Rect(0, 0, width, height), pixels, stride, 0);
+            wb.WritePixels(new Int32Rect(0, 0, BMP_WIDTH, BMP_HEIGHT), pixels, stride, 0);
             return wb;
         }
 
+        /// <summary>
+        /// 色同期処理です。
+        /// </summary>
         protected override void SyncColorCore(string propertyName)
         {
             _colorUpdater.Update(() =>
@@ -126,8 +122,6 @@ namespace ColorPicker
 
                 Canvas.SetLeft(Current, location.X - 8.0);
                 Canvas.SetTop(Current, location.Y - 8.0);
-                _brush.Color = CurrentColor;
-                _brushBaseColor.GradientStops[1].Color = BaseColor;
             });
         }
     }
