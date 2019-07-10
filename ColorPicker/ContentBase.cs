3322985d6d9622d7a953056a38fd3805f001cff8
nenaaki <nenaaki@gmail.com>
2019-07-11 00:51:05 +0900

    コンボボックス作りかけ コメント補強

--- ColorPicker/ContentBase.cs
+++ ColorPicker/ContentBase.cs
@@ -4,10 +4,25 @@ using System.Runtime.CompilerServices;
 
 namespace ColorPicker
 {
+    /// <summary>
+    /// 通知機能の基底クラスです。
+    /// </summary>
     public abstract class ContentBase : INotifyPropertyChanged
     {
+        /// <summary>
+        /// プロパティの変更を通知します。
+        /// </summary>
         public event PropertyChangedEventHandler PropertyChanged;
 
+        /// <summary>
+        /// プロパティに変化があった場合に<see cref="PropertyChanged"/>イベントを発生します。
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="field"></param>
+        /// <param name="value"></param>
+        /// <param name="propertyName"></param>
+        /// <param name="dependedProperties"></param>
+        /// <returns>変更の有無</returns>
         public bool UpdateProperty<T>(ref T field, T value, [CallerMemberName]string propertyName = null, string[] dependedProperties = null)
             where T : IEquatable<T>
         {
@@ -19,6 +34,10 @@ namespace ColorPicker
             return true;
         }
 
+        /// <summary>
+        /// <see cref="ProerptyChanged"/>を発生します。
+        /// </summary>
+        /// <param name="propertyName">プロパティから使用する場合はnullにしてください</param>
         public void OnPropertyChanged([CallerMemberName]string propertyName = null) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 }
