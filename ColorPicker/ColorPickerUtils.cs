29e034ee905296bed1c76ff1f1ece41bbec5fb15
nenaaki <nenaaki@gmail.com>
2019-07-11 07:01:50 +0900

    コンボボックスStyle作成中

--- ColorPicker/ColorPickerUtils.cs
+++ ColorPicker/ColorPickerUtils.cs
@@ -4,8 +4,14 @@ namespace ColorPicker
 {
     internal static class ColorPickerUtils
     {
+        /// <summary>
+        /// 共有する黒いペンです。
+        /// </summary>
         public static readonly Pen BlackPen = new Pen(Brushes.Black, 1);
 
+        /// <summary>
+        /// 共有する白いペンです。
+        /// </summary>
         public static readonly Pen WhitePen = new Pen(Brushes.White, 1);
 
     }
