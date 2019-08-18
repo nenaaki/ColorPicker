f97432e1ba38c3b34ec1e5b540e430c15ed38805
nenaaki <nenaaki@gmail.com>
2019-08-19 00:40:38 +0900

    コンボボックス完成

---
+++ ColorPicker/Converters/WhiteOrBlackConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+using System.Globalization;
+
+namespace Oniqys.Wpf.Controls.ColorPicker.Converters
+{
+    public class WhiteOrBlackConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) => value is Color color ? HsvColor.FromColor(color).GetBrightness() > 0.5 ? Colors.Black : Colors.White : Colors.Black;
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotSupportedException();
+    }
+}
